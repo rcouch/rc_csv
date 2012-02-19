ec4a8d0e30269de1f5619682c157b7e5128a50c1
Nicolas R Dufour <nrdufour@nemoworld.info>
2012-02-19 12:56:55 -0500

    OK the piping is working.

--- src/refuge_csv_http.erl
+++ src/refuge_csv_http.erl
@@ -5,5 +5,22 @@
 -module(refuge_csv_http).
 -author("Nicolas R Dufour <nicolas.dufour@nemoworld.info>").
 
+-export([handle_csv_req/2]).
+
 -include_lib("couch/include/couch_db.hrl").
+
+handle_csv_req(#httpd{method='POST'}=Req, Db) ->
+    csv_req(Req, Db);
+handle_csv_req(Req, _Db) ->
+    couch_httpd:send_method_not_allowed(Req, "POST,HEAD").
+
+%%
+
+csv_req(Req, Db) ->
+    ?LOG_DEBUG("csv was called with Req=~p and Db=~p!", [Req, Db]),
+
+    do_csv_req(Req, Db).
+
+do_csv_req(Req, Db) ->
+    {ok, []}.
 
