756b4184d0624d86d458008a97ea3e97709ccef0
Nicolas R Dufour <nrdufour@nemoworld.info>
2012-03-16 08:40:00 -0400

    Working document creation from each csv row.

--- src/refuge_csv.erl
+++ src/refuge_csv.erl
@@ -12,8 +12,15 @@
     import_csv_in_db/2
 ]).
 
+-record(pstate, {
+    db,
+    headers = []
+}).
+
 import_csv_in_db(Db, Args) ->
-    InitState = ecsv_parser:init(fun process_csv_row/2, []),
+    InitProcessingState = #pstate{db=Db},
+
+    InitState = ecsv_parser:init(fun process_csv_row/2, InitProcessingState),
 
     StreamingFun = Args#rcargs.streaming_fun,
     StreamingFun(fun process_chunk/2, InitState),
@@ -25,9 +32,44 @@ import_csv_in_db(Db, Args) ->
 process_csv_row({eof}, State) ->
     ?LOG_DEBUG("The stread has ended!", []),
     State;
-process_csv_row({newline, NewLine}, State) ->
+process_csv_row({newline, NewLine}, #pstate{headers=Headers, db=Db}=State) ->
     ?LOG_DEBUG("I got a newLine: ~p", [NewLine]),
-    State.
+
+    case Headers of
+        [] ->
+            State#pstate{headers=NewLine};
+        _  ->
+            % create a document from the header and the new row
+            {ok, InitialDoc} = make_initial_doc(Headers, NewLine),
+
+            % then transform it (if needed)
+            % TODO put the transformation here
+            % for now, no transformation yet
+            TransformedDoc = InitialDoc,
+
+            % and finally write the doc
+            {ok, _Rev} = couch_db:update_doc(Db, TransformedDoc, []),
+
+            State
+    end.
+
+make_initial_doc(Headers, Row) ->
+    Props = lists:zipwith(
+        fun(K, V) ->
+            {iolist_to_binary(K), iolist_to_binary(V)}
+        end,
+        Headers,
+        Row
+    ),
+
+    IdProp = {<<"_id">>, couch_uuids:new()},
+    PropsWithId = [IdProp|Props],
+
+    ?LOG_DEBUG("New Document: ~p", [PropsWithId]),
+
+    {ok, couch_doc:from_json_obj({PropsWithId})}.
+
+% -----------------------------------------------------------------------------
 
 process_chunk({eof}, State) ->
     ecsv_parser:end_parsing(State);
